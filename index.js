f12bb2d308f76c9d1e71f203ab9d119c7ff92ea4
PedroVitor-oss <pedro.santos22@escola.pr.edu.br>
2022-06-18 17:55:50 -0300

    login

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let app = express();
 let port = process.env.PORT||3000
 
 app.get('/',(req,res)=>{
-    res.sendFile('/views/login.html')})
+    res.sendFile(__dirname+'/views/login.html')})
 app.get('/chat',(req,res)=>{
     res.sendFile('/views/chat.html')
 })
