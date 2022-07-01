90af181716bc3daf49e1e0a88109b247768a600c
PedroVitor-oss <pedro.santos22@escola.pr.edu.br>
2022-07-01 10:27:12 -0300

    melhora do login

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ let app = express();
 let port = process.env.PORT||3000
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/views/login.html')})
+    res.sendFile(__dirname+'/views/singUp.html')})
 app.get('/chat',(req,res)=>{
-    res.sendFile('/views/chat.html')
+    res.sendFile(__dirname+'/views/chat.html')
 })
+
 
 app.listen(port,()=>{
     console.log('O SERVIDOR ESTA RODANDO NA PORTA '+port+' ...')
