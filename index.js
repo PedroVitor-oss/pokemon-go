5e3bb7c0235eeefd1cb2c9f96b7e3bece6fb838a
PedroVitor-oss <pedro.santos22@escola.pr.edu.br>
2022-07-06 17:05:24 -0300

    baixando imagens

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ let app = express();
 let port = process.env.PORT||3000
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/views/singUp.html')})
+    res.sendFile(__dirname+'/views/singUp.html')});
+app.get('/baixarArquivo',(req,res)=>{
+    res.sendFile(__dirname+'/views/baixarArquivo.html')}
+    )
 app.get('/chat',(req,res)=>{
     res.sendFile(__dirname+'/views/chat.html')
 })
